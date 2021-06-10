a5eaebe8ba0ecde2a74002fd6a1d4fc307fa4cf7
muthu-kumaravel <muthukumaravel.muthuraman@gmail.com>
2021-06-10 14:41:07 +0530

    ADD : Basic pages with print validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,44 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Routes, RouterModule } from '@angular/router';
+
+import { LandingComponent } from './landing/landing.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'landing/home',
+    pathMatch: 'full'
+  },
+  {
+    path : 'landing',
+    component : LandingComponent,
+    canActivate : [],
+    children : [
+      {
+        path: 'home',
+        component: HomeComponent,
+        canActivate: []
+      }
+    ]
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
